16ae3ec5d68d3dbdcf99abfe6179c9e1378c5374
simoncollin <simon.collin.auditeur@lecnam.net>
2020-03-29 21:18:43 +0200

    first commit

---
+++ src/main/java/com/example/tuto/DatabaseLoader.java
@@ -0,0 +1,22 @@
+package com.example.tuto;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseLoader implements CommandLineRunner {
+
+    private final EmployeeRepository repository;
+
+    @Autowired
+    public DatabaseLoader(EmployeeRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        this.repository.save(new Employee("Simon", "Collin", "dev nul"));
+        this.repository.save(new Employee("Alix", "Leger", "dev pas nul"));
+    }
+}
---
+++ src/main/java/com/example/tuto/TutoApplication.java
@@ -0,0 +1,14 @@
+package com.example.tuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TutoApplication.class, args);
+	}
+
+}
